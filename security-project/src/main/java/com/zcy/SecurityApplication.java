f0e3c4b992df3621d4ffa5c00c5d906097f6068d
zhuangchongyi <1909017815@qq.com>
2020-06-30 22:00:49 +0800

    配置redis session共享

--- security-project/src/main/java/com/zcy/SecurityApplication.java
+++ security-project/src/main/java/com/zcy/SecurityApplication.java
@@ -3,8 +3,11 @@ package com.zcy;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
 @MapperScan("com.zcy.**.dao")
 @SpringBootApplication
+@EnableRedisHttpSession
 public class SecurityApplication {
 
     public static void main(String[] args) {
